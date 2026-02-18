1952be636e370c2a5f1662a84c8d4a1c1f9ccafe
gilad45 <giladberkove10@gmail.com>
2026-02-18 18:30:17 +0200

    changed to non gui joint pub

--- src/robot_launch/launch/robot_launch.launch.py
+++ src/robot_launch/launch/robot_launch.launch.py
@@ -74,9 +74,9 @@ def generate_launch_description():
     )
 
     robot_joint_publisher = Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui'
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher'
     )
 
     ld.add_action(robot_state_publisher)
